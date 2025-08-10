0b5cf8185365bdc592f8d18962504c1665648b67
elfffffy <124286592+elfffffy@users.noreply.github.com>
2025-08-11 00:25:46 +0900

    [D2] Title: Crazy Arcade - Low, Time: 55 ms, Memory: 51,200 KB -BaekjoonHub

--- SWEA/D2/24941. Crazy Arcade － Low/Crazy Arcade － Low.py
+++ SWEA/D2/24941. Crazy Arcade － Low/Crazy Arcade － Low.py
@@ -1,8 +1,10 @@
+"""
+
+"""
 T = int(input())
 
 
-def water_shoot(y, x):
-    # 상 하 좌 우
+def catch(y, x):
     dy = [-1, 1, 0, 0]
     dx = [0, 0, -1, 1]
     sum_v = arr[y][x]
@@ -10,36 +12,23 @@ def water_shoot(y, x):
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
+        if ny < 0 or nx < 0 or nx >= N or ny >= N: continue
 
-        # 물풍선 모양 '+' 안되면 break
-        if ny < 0 or nx < 0 or ny >= N or nx >= N: return 0
-
-        # 값 비교
         if arr[y][x] <= arr[ny][nx]:
-            return 0
-        else:
-            sum_v += arr[ny][nx]
+            break
 
-
-    # 모든 조건을 충족 했을 경우
-    else:
-        return sum_v
+        sum_v += arr[ny][nx]
+    return sum_v
 
 
 for tc in range(1, T + 1):
-    # 맵의 크기
     N = int(input())
-
-    # 맵의 점수
     arr = [list(map(int, input().split())) for _ in range(N)]
-
-    max_v = 0
-
+    max_v = float('-inf')
     for y in range(N):
         for x in range(N):
-            result = water_shoot(y, x)
-
-            # 물풍선 값 중 제일 큰 값
-            max_v = max(max_v, result)
+            result = catch(y, x)
+            if result > max_v:
+                max_v = result
 
     print(f'#{tc} {max_v}')
