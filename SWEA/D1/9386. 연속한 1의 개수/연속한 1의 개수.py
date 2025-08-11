9c907b973ae237396fd12cdf87b958e110ca75c2
elfffffy <124286592+elfffffy@users.noreply.github.com>
2025-08-11 22:39:13 +0900

    [D1] Title: 연속한 1의 개수, Time: 66 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
+++ SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
@@ -1,28 +1,17 @@
 T = int(input())
 
 for tc in range(1, T + 1):
-    # 수열의 길이
     N = int(input())
-
-    # 0과 1로 구성된 수열
-    num = input()
+    nums = input()
 
     cnt = 0
-    max_v = float('-inf')
+    max_v = 0
 
-    for x in num:
-        # 0일 때 실행
-        if x == '0':
-            # 지금까지의 연속된 1의 개수와 최댓값 비교 후 갱신
-            max_v = max(cnt, max_v)
-            cnt = 0
-            continue
-
-        # 1일 때 실행
-        else:
+    for num in nums:
+        if num == '1':
             cnt += 1
-
-    # 수열이 1로 끝나는 경우를 대비해 최종 갱신
-    max_v = max(cnt, max_v)
+            max_v = max(cnt, max_v)
+        else:
+            cnt = 0
 
     print(f'#{tc} {max_v}')
