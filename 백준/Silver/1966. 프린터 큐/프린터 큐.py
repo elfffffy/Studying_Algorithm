96f73799eb91f050c987d245fabcac03ec6f286d
elfffffy <124286592+elfffffy@users.noreply.github.com>
2025-08-27 00:03:15 +0900

    [Silver III] Title: 프린터 큐, Time: 76 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    num_list = list(map(int, input().split()))
+    q = deque()
+    for idx, num in enumerate(num_list):
+        q.append((num, idx))
+
+    cnt = 0
+    target_idx = q[M][1]
+
+    while q:
+        max_v = max(q)[0]
+
+        now = q[0]
+        if now[0] < max_v:
+            q.append(q.popleft())
+        else:
+            q.popleft()
+            cnt += 1
+            if target_idx == now[1]:
+                break
+
+    print(cnt)
