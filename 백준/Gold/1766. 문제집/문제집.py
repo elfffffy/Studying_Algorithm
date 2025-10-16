d98f5ebebb3c7d8104f8a45c462e5742d3f7ce1c
elfffffy <124286592+elfffffy@users.noreply.github.com>
2025-10-16 23:41:56 +0900

    [Gold II] Title: 문제집, Time: 2668 ms, Memory: 42736 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,30 @@
+"""
+백준 - 문제집
+"""
+import heapq
+from collections import deque
+
+# N : 문제의 수
+# M : 정보의 개수
+N, M = map(int, input().split())
+in_degree = [0] * (N + 1)
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    in_degree[b] += 1
+    graph[a].append(b)
+
+pq = []
+
+for i in range(1, N + 1):
+    if in_degree[i] == 0:
+        heapq.heappush(pq, i)
+
+while pq:
+    c_node = heapq.heappop(pq)
+    print(c_node, end=" ")
+    for n_node in graph[c_node]:
+        in_degree[n_node] -= 1
+        if in_degree[n_node] == 0:
+            heapq.heappush(pq, n_node)
