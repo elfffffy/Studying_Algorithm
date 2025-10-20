aae5b01f508be0c41e7c758fa8086d97c2ba3916
elfffffy <124286592+elfffffy@users.noreply.github.com>
2025-10-21 00:09:54 +0900

    [Gold IV] Title: 플로이드, Time: 236 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -5,22 +5,30 @@ import sys
 
 input = sys.stdin.readline
 
+
+def solve():
+    global MAP
+
+    for i in range(1, N + 1):
+        MAP[i][i] = 0
+
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        MAP[a][b] = min(MAP[a][b], c)
+
+    for mid in range(1, N + 1):
+        for start in range(1, N + 1):
+            for end in range(1, N + 1):
+                MAP[start][end] = min(MAP[start][end], MAP[start][mid] + MAP[mid][end])
+
+    return MAP
+
+
 N = int(input())
 M = int(input())
 INF = float('inf')
 MAP = [[INF] * (N + 1) for _ in range(N + 1)]
-
-for i in range(1, N + 1):
-    MAP[i][i] = 0
-
-for _ in range(M):
-    a, b, c = map(int, input().split())
-    MAP[a][b] = min(MAP[a][b], c)
-
-for mid in range(1, N + 1):
-    for start in range(1, N + 1):
-        for end in range(1, N + 1):
-            MAP[start][end] = min(MAP[start][end], MAP[start][mid] + MAP[mid][end])
+dist = solve()
 
 for y in range(1, N + 1):
     for x in range(1, N + 1):
