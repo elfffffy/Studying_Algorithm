e13570310514ca71ba9d393bb67304d6da8e7cb4
elfffffy <124286592+elfffffy@users.noreply.github.com>
2025-10-21 00:24:35 +0900

    [Gold IV] Title: 플로이드, Time: 184 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -6,34 +6,37 @@ import sys
 input = sys.stdin.readline
 
 
+def print_func(N, MAP):
+    for y in range(1, N + 1):
+        for x in range(1, N + 1):
+            if MAP[y][x] == float('inf'):
+                print(0, end=" ")
+            else:
+                print(MAP[y][x], end=" ")
+        print()
+
+
 def solve():
-    global MAP
+    N = int(input())
+    M = int(input())
+    INF = float('inf')
+    MAP = [[INF] * (N + 1) for _ in range(N + 1)]
 
     for i in range(1, N + 1):
         MAP[i][i] = 0
 
     for _ in range(M):
         a, b, c = map(int, input().split())
-        MAP[a][b] = min(MAP[a][b], c)
+        if MAP[a][b] > c:
+            MAP[a][b] = c
 
     for mid in range(1, N + 1):
         for start in range(1, N + 1):
             for end in range(1, N + 1):
-                MAP[start][end] = min(MAP[start][end], MAP[start][mid] + MAP[mid][end])
+                if MAP[start][end] > MAP[start][mid] + MAP[mid][end]:
+                    MAP[start][end] = MAP[start][mid] + MAP[mid][end]
 
-    return MAP
+    print_func(N, MAP)
 
 
-N = int(input())
-M = int(input())
-INF = float('inf')
-MAP = [[INF] * (N + 1) for _ in range(N + 1)]
-dist = solve()
-
-for y in range(1, N + 1):
-    for x in range(1, N + 1):
-        if MAP[y][x] == float('inf'):
-            print(0, end=" ")
-        else:
-            print(MAP[y][x], end=" ")
-    print()
+solve()
