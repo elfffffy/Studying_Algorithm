f6dd15d5099fd6270781a39f9e7094215126e39c
elfffffy <124286592+elfffffy@users.noreply.github.com>
2025-12-17 13:04:06 +0900

    [Gold IV] Title: 네트워크 연결, Time: 2776 ms, Memory: 55240 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,37 @@
+"""
+네트워크 연결
+"""
+
+
+def Find(x):
+    if boss[x] == x:
+        return x
+    boss[x] = Find(boss[x])
+    return boss[x]
+
+
+def Union(a, b):
+    t1 = Find(a)
+    t2 = Find(b)
+
+    if t1 == t2: return
+    boss[t2] = t1
+
+
+N = int(input())
+M = int(input())
+connections = [list(map(int, input().split())) for _ in range(M)]
+
+# 비용을 기준으로 정렬
+# (a 컴퓨터, b 컴퓨터, 연결 비용)
+connections.sort(key=lambda x: x[-1])
+
+# Union-Find
+total_cost = 0
+boss = [x for x in range(N + 1)]
+for a, b, c in connections:
+    if Find(a) == Find(b): continue
+    Union(a, b)
+    total_cost += c
+
+print(total_cost)
